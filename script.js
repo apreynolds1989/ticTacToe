3d761df54116be8ed2cfe8c17a1085e851d18989
apreynolds1989 <apreynolds1989@gmail.com>
2022-04-02 01:44:41 -0230

    Created X and O in CSS and currently applied to cells 1 and 2, created Event Listener for button click and created clickButton function
    clickButton function currently not working

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log('hellow world');
+// Function to increase turn counter
+let turnCounter = 1;
+const nextTurn = () => turnCounter++;
+
+// Function for clicking a button and showing result
+function clickButton() {
+    const element = document.querySelector(cell);
+    if (turnCounter % 2) {
+        element.classList.add('circle');
+    } else {
+        element.classList.add('x');
+    };
+    nextTurn();
+};
+
+// Button click Event Listener
+const wrapper = document.querySelector('wrapper');
+
+/* wrapper.addEventListener('click', (event) => {
+    const isButton = event.target.nodeName === 'BUTTON';
+    if (!isButton) {
+        return;
+    }
+    clickButton();
+}); */
+const buttons = document.querySelectorAll('button[id^=btn]');
+
+buttons.forEach(button => {
+    button.addEventListener('click', event => {
+        clickButton();
+    });
+});
